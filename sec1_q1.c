60d9432883696c8346bbb8ece77f78c74cb8413a
Laiba-17A <laiba17javed@gmail.com>
2024-11-27 23:43:51 +0500

    Update sec1_q1.c

--- sec1_q1.c
+++ sec1_q1.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main(){
+	int n;
+	printf("enter any number");
+	scanf("%d",&n);
+	printf("the sum is:%d",sum(n));
+}
+int sum(int n){
+	int x,a;
+	if(n==0){
+		return 0;
+	}
+	return n%10 + sum(n/10);
+}
